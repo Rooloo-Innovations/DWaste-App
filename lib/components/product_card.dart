cf57bcd4937cbd467879300923ac540882a7c89e
raeesnazeer <raeesnazeer@gmail.com>
2023-03-31 14:30:28 +0530

    product list screen added

---
+++ lib/components/product_card.dart
@@ -0,0 +1,107 @@
+import 'package:dwaste/models/app_colors.dart';
+import 'package:dwaste/screens/product_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({
+    super.key,
+    required this.image,
+    required this.name,
+    required this.price,
+    this.onTap,
+  });
+
+  final String image;
+  final String name;
+  final int price;
+  final onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: InkWell(
+        onTap: () {
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => const ProductScreen(),
+          ));
+        },
+        child: Container(
+          decoration: BoxDecoration(boxShadow: [
+            BoxShadow(color: Colors.black.withOpacity(0.1), blurRadius: 6),
+          ], color: AppColors.white, borderRadius: BorderRadius.circular(10)),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Expanded(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: SizedBox(
+                      height: 100,
+                      child: Image.network(
+                        image,
+                        fit: BoxFit.cover,
+                        errorBuilder: (BuildContext context, Object exception,
+                            StackTrace? stackTrace) {
+                          return Image(
+                              image:
+                                  AssetImage('assets/images/placeholder.png'));
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Text(
+                        name,
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: AppColors.black,
+                          fontWeight: FontWeight.w600,
+                        ),
+                        softWrap: true,
+                        maxLines: 2,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Row(
+                      children: [
+                        SvgPicture.asset(
+                          'assets/images/icons/DIcon.svg',
+                          width: 16,
+                          height: 16,
+                          colorFilter: const ColorFilter.mode(
+                              AppColors.green, BlendMode.srcIn),
+                        ),
+                        const SizedBox(
+                          width: 2,
+                        ),
+                        Text(
+                          price.toString(),
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                              color: AppColors.green),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/product_list_screen.dart
+++ lib/screens/product_list_screen.dart
@@ -1,5 +1,9 @@
 import 'package:dwaste/models/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+import '../components/product_card.dart';
+import '../models/gql_client.dart';
 
 class ProductListScreen extends StatefulWidget {
   const ProductListScreen({Key? key}) : super(key: key);
@@ -9,86 +13,104 @@ class ProductListScreen extends StatefulWidget {
 }
 
 class _ProductListScreenState extends State<ProductListScreen> {
+  List productsList = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchProductsbyCategoryId();
+  }
+
+  void fetchProductsbyCategoryId() async {
+    final GraphQLClient client = await getClient();
+
+    final QueryOptions options = QueryOptions(document: gql(r'''
+query AllProductsByCategory($subCategoryId: String!) {
+  allProductsByCategory(subCategoryID: $subCategoryId) {
+    success
+    message
+    products {
+      id
+      name
+      actualPrice
+      discountedPrice
+      imageURL
+    }
+  }
+}
+    '''), variables: {"subCategoryId": "640c29be27c311751b05ee70"});
+
+    final QueryResult result = await client.query(options);
+
+    if (result.hasException) {
+      throw result.exception.toString();
+    } else {
+      setState(() {
+        productsList = result.data!['allProductsByCategory']['products'];
+        print(productsList);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
-        child: Column(
-          children: [
-            Expanded(
+      body: Column(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 0),
               child: GridView.count(
                 primary: false,
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-                crossAxisCount: 3,
-                children: <Widget>[
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.teal[100],
-                    child: Column(
-                      children: [
-                        Image.asset('assets/images/headphones.png'),
-                        const Text("Headphones"),
-                      ],
-                    ),
+                crossAxisCount: 2,
+                childAspectRatio: 2 / 3,
+                children: List<Widget>.generate(
+                  productsList.length,
+                  (index) => ProductCard(
+                    name: productsList[index]['name'],
+                    price: productsList[index]['discountedPrice'],
+                    image: productsList[index]['imageURL'],
                   ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.teal[200],
-                    child: const Text('Heed not the rabble'),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.teal[300],
-                    child: const Text('Sound of screams but the'),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.teal[400],
-                    child: const Text('Who scream'),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.teal[500],
-                    child: const Text('Revolution is coming...'),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.teal[600],
-                    child: const Text('Revolution, they...'),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.teal[200],
-                    child: const Text('Heed not the rabble'),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.teal[300],
-                    child: const Text('Sound of screams but the'),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.teal[400],
-                    child: const Text('Who scream'),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.teal[500],
-                    child: const Text('Revolution is coming...'),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.teal[600],
-                    child: const Text('Revolution, they...'),
-                  ),
-                ],
+                ),
+
+                // children: <Widget>[
+                //   ProductCard(
+                //     name: 'Headphones',
+                //     price: 10,
+                //     image: 'assets/images/headphone.png',
+                //   ),
+                //   ProductCard(
+                //     name: 'Headphone',
+                //     price: 10,
+                //     image: 'assets/images/headphone.png',
+                //   ),
+                //   ProductCard(
+                //     name: 'Headphone',
+                //     price: 10,
+                //     image: 'assets/images/headphone.png',
+                //   ),
+                //   ProductCard(
+                //     name: 'Headphone',
+                //     price: 10,
+                //     image: 'assets/images/headphone.png',
+                //   ),
+                //   ProductCard(
+                //     name: 'Headphone',
+                //     price: 10,
+                //     image: 'assets/images/headphone.png',
+                //   ),
+                //   ProductCard(
+                //     name: 'Headphone',
+                //     price: 10,
+                //     image: 'assets/images/headphone.png',
+                //   ),
+                // ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
